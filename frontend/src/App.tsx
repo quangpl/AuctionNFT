05c2de2ec0825bb1b1d0664db555b8a88c7590a2
hoangxt2803 <hoangxt2803@gmail.com>
2021-12-11 14:49:19 +0700

    - update frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,7 @@
 import React, {Component, Fragment } from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import "antd/dist/antd.css";
 import {BrowserRouter, Route, Routes} from 'react-router-dom'
 import Home from "./components/home"
 class App extends Component {
@@ -9,7 +9,7 @@ class App extends Component {
     return (
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Home/>}>
+          <Route path="/sample" element={<Home/>}>
           </Route>
         </Routes>
       </BrowserRouter>
