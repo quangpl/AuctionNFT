0748d81f5bbc406af84088952f35e19307782b7d
Dong Viet Tung <minhmang456@gmail.com>
2021-12-29 17:32:31 +0700

    add authentication

--- backend/src/controllers/index.ts
+++ backend/src/controllers/index.ts
@@ -1,6 +1,8 @@
 import { Request, Response } from "express";
 import {User} from "../models/User";
-
+import { requireJwtMiddleware } from "../middlewares/authentication";
+import { encodeSession } from "../helpers/jwtHelpers";
+import { isEmpty } from "lodash";
 /**
  * GET /
  * Home page.
@@ -12,7 +14,16 @@ export const index = async (req: Request, res: Response): Promise<void> => {
 
 export const getAllUsers = async (req: Request, res: Response): Promise<void> => {
   const users: any = await User.find()
-  res.render("test", {data: users})
+  if(!isEmpty(users)) {
+    res.status(200).json({
+      data: users
+    })
+  } else {
+    res.status(401).json({
+      message: "User not found"
+    })
+  }
+  // res.render("test", {data: users})
 };
 
 export const createUser = async (req: Request, res: Response): Promise<void> => {
@@ -21,15 +32,31 @@ export const createUser = async (req: Request, res: Response): Promise<void> => 
     password: req.body.password,
     email: req.body.email
   })
-  res.render("test", {data: user})
+  res.status(200).json({
+    message: "User created",
+    data: user
+  })
+  // res.render("test", {data: user})
 };
 
 export const getUserById = async (req: Request, res: Response): Promise<void> => {
   try {
     const user: any = await User.findById(req.params.id)
-    res.render("test", {data: user})
+    if(!isEmpty(user)) {
+    // res.render("test", {data: user})
+    res.status(200).json({
+      message: "OK",
+      data: user
+    })
+    } else {
+      res.status(401).json({
+        message: "User not found"
+      })
+    }
   } catch {
-    res.render("error", {title: "Error", message: "User not found"})
+    res.status(500).json({
+      message: "Something wrong"
+    })
   }
 };
 
@@ -37,21 +64,60 @@ export const updateUserById = async (req: Request, res: Response): Promise<void>
   try {
     const user: any = await User.findById(req.params.id)
 
-    if(req.body.username) user.username = req.body.username
-    if(req.body.password) user.password = req.body.password
-    if(req.body.email) user.email = req.body.email
+    if(!isEmpty(user)) {
+      if(req.body.username) user.username = req.body.username
+      if(req.body.password) user.password = req.body.password
+      if(req.body.email) user.email = req.body.email
 
-    res.render("test", {data: user})
+      res.status(200).json({
+        message: "User updated",
+        data: user
+      })
+    } else {
+      res.status(401).json({
+        message: "User not found"
+      })
+    }
   } catch {
-    res.render("error", {title: "Error", message: "User not found"})
+    res.status(500).json({
+      message: "Something wrong"
+    })
   }
 };
 
 export const deleteUserById = async (req: Request, res: Response): Promise<void> => {
   try {
     await User.findByIdAndDelete(req.params.id)
-    res.render("test", {data: "Delete OK"})
+    // res.render("test", {data: "Delete OK"})
+    res.status(200).json({
+      message: "Delete OK",
+    })
   } catch {
-    res.render("error", {title: "Error", message: "User not found"})
+    res.status(500).json({
+      message: "Something wrong"
+    })
   }
 };
+
+export const login = async (req: Request, res: Response): Promise<void> => {
+  try {
+    const user: any = await User.find({username: req.body.username, password: req.body.password}).exec();
+    if(!isEmpty(user)) {
+      const session = encodeSession(process.env.JWT_SECRET_KEY, {
+        username: user.username,
+        id: user.id
+      });
+
+      res.status(201).json(session);
+    } else {
+      res.status(401).json({
+        message: "Username or password is wrong"
+      })
+    }
+
+  } catch {
+    res.status(500).json({
+      message: "Something wrong"
+    })
+  }
+}
